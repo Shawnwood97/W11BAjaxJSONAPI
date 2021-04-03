9904b4dfe765e4bdf6b2d9b00624163240595a5e
Shawn Wood <Shawnwood97@gmail.com>
2021-04-03 00:17:14 -0600

    finished, little late because of workiong on bonus. all working now.

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-// https://pipl.ir/v1/getPerson
+// if statements in function changed to provide error if returned readyState and status are not correct.
 
-// function getNewPerson() {}
+function generatePerson() {
+  personWrapper.innerHTML = `<div id="loading">Loading User!</div>`;
+  let ajax = new XMLHttpRequest();
+  ajax.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      if (this.status == 200) {
+        object = JSON.parse(this.responseText);
+        person = object.results[0];
+        console.log(person);
 
-let ajax = new XMLHttpRequest();
-ajax.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    console.log(this.responseText);
-  }
-};
+        let personPhoto = person.picture.large;
+        let personFirstName = person.name.first;
+        let personLastName = person.name.last;
+        let personTitle = person.name.title;
 
-ajax.open("GET", "https://randomuser.me/api/", true);
-ajax.send();
+        // add simple innerHTML to the page whn button is clicked (below function)
+        personWrapper.innerHTML = `<img id ="photo" src =${personPhoto} /> `;
+        personWrapper.innerHTML += `<h3 id ="full_name">${personTitle}. ${personFirstName} ${personLastName}</h3> `;
+      } else {
+        personWrapper.innerHTML = `<div id="loading">Error, try again later</div>`;
+      }
+    }
+  };
+  ajax.open("GET", "https://randomuser.me/api/", true);
+  // this is how I tested for an error - WORKED
+  // ajax.open("GET", "https://thiswebsiteisfaketotest.error/api/", true);
+  ajax.send();
+}
+
+let personWrapper = document.getElementById("person");
+
+let genButton = document.getElementById("generate_person");
+genButton.addEventListener("click", generatePerson);
